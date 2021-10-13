b63ec149945959a9764f8f9b5abaa5db0f108717
KaydunovDenis <deniskaydunov@gmail.com>
2021-10-13 16:45:40 +0300

    have issues

---
+++ bot-on-gradle/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Application {
+
+	public static void main(String[] args) {
+		ApiContextInitializer.init();
+		SpringApplication.run(Application.class, args);
+	}
+
+}
