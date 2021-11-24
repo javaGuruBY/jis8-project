e2a52eb3c5042352a2330aacdafa8ad08e8a3a53
lamaconda <kennethoffi6.6@gmail.com>
2021-11-24 22:56:13 +0300

    adds script that counts max available slots dynamicly

--- register-service/src/main/resources/sql/20211124_01_classes_with_max_available_slots.sql
+++ register-service/src/main/resources/sql/20211124_01_classes_with_max_available_slots.sql
@@ -6,4 +6,24 @@ from (SELECT fitness_class_name
                LEFT JOIN slot_to_user stu on slot.id = stu.slot_id
       WHERE user_id IS NULL) as slot
 GROUP BY fitness_class_name
-ORDER BY available_slot DESC;
+ORDER BY available_slot desc;
+
+
+#58-1 find 3d max available slots classes, dynamic
+SELECT fitness_class_name, COUNT(fitness_class_name) as available_slot
+from (SELECT fitness_class_name
+      FROM slot
+               LEFT JOIN fitness_class fc on slot.fitness_class_id = fc.fitness_class_id
+               LEFT JOIN slot_to_user stu on slot.id = stu.slot_id
+      WHERE user_id IS NULL) as slot
+GROUP BY fitness_class_name
+HAVING available_slot =
+       (SELECT COUNT(fitness_class_name) as available_slot
+        from (SELECT fitness_class_name
+              FROM slot
+                       LEFT JOIN fitness_class fc on slot.fitness_class_id = fc.fitness_class_id
+                       LEFT JOIN slot_to_user stu on slot.id = stu.slot_id
+              WHERE user_id IS NULL) as slot
+        GROUP BY fitness_class_name
+        ORDER BY available_slot desc
+        LIMIT 2, 1);
