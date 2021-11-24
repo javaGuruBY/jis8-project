8b96931749cfb4b063a3a390711baa041dc0c593
lamaconda <kennethoffi6.6@gmail.com>
2021-11-24 21:46:17 +0300

    adds script that counts free slots for selected month

---
+++ register-service/src/main/resources/sql/20211124_02_free_slots_for_selected_month.sql
@@ -0,0 +1,8 @@
+#60 count free slots for selected month
+SELECT COUNT(MONTH(start))
+from (SELECT datetime as start
+      FROM slot
+               LEFT JOIN fitness_class fc on slot.fitness_class_id = fc.fitness_class_id
+               LEFT JOIN slot_to_user stu on slot.id = stu.slot_id
+      WHERE user_id IS NULL) as slot
+WHERE MONTH(start) = 12;
